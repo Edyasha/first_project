f81ca28e6eaf94cf9e6d4ef79af17cb3889ba0df
Edvard <edyasha00@gmail.com>
2022-02-18 09:22:55 +0200

    styleN

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/Counter';
+import './styles/app.css';
 
 function App() {
   const [value, setValue] = useState('Текст в инпуте')
@@ -8,7 +9,18 @@ function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      <ClassCounter />
+      {/* <ClassCounter /> */}
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - язык программирования
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
