4c67474f6a6f95c7f84c536938db7144a600f089
Edvard <edyasha00@gmail.com>
2022-02-21 12:58:45 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -14,11 +14,13 @@ function App() {
     {id: 3, title: 'React', body: 'Description'},
   ]);
 
-  const [title, setTitle] = useState('')
+  const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
 
   const addNewPost = (e) => {
     e.preventDefault()
     console.log(title)
+    console.log(bodyInputRef.current.value)
   }
 
   return (
@@ -28,8 +30,13 @@ function App() {
           value={title}
           onChange={e => setTitle(e.target.value)}
           type="text" 
-          placeholder="Название поста"/>
-        <MyInput type="text" placeholder="Описание поста"/>
+          placeholder="Название поста"
+        />
+        <MyInput 
+          ref={bodyInputRef}
+          type="text" 
+          placeholder="Описание поста"
+        />
         <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title={'Посты про JS'} />
