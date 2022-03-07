32ae7057426a4d70659da8effd0b8a7d6422642f
Edvard <edyasha00@gmail.com>
2022-03-07 15:17:11 +0200

    fon

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import cl from './Dialogs.module.css';
+import React from "react";
+import cl from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={cl.dialogs}>
+      <div className={cl.dialogs_item}>
+        <div className={`${cl.dialog} ${cl.active}`}>Edison</div>
+        <div className={cl.dialog}>Maksim</div>
+        <div className={cl.dialog}>Svetlana</div>
+        <div className={cl.dialog}>Viktor</div>
+        <div className={cl.dialog}>Michael</div>
+        <div className={cl.dialog}>Valeriy</div>
+      </div>
+      <div className={cl.messages}>
+        <div className={cl.message}>Hi!</div>
+        <div className={cl.message}>How is your life&</div>
+        <div className={cl.message}>You!</div>
+      </div>
     </div>
   );
-}
+};
 
 export default Dialogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className={cl.header}>
       <header>
-        <img src="https://uploads.turbologo.com/uploads/design/preview_image/2760025/preview_image20210122-21148-1eoksow.png" />
+        {/* <img src="https://uploads.turbologo.com/uploads/design/preview_image/2760025/preview_image20210122-21148-1eoksow.png" /> */}
       </header>
     </div>
   );
