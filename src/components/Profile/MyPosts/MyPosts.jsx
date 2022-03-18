1df0a9c60ecbd475540d5e99ecc05549d55b51ad
Edvard <edyasha00@gmail.com>
2022-03-18 14:34:53 +0200

    ref

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,28 +3,25 @@ import cl from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-  // let posts = [
-  //   { id: 1, message: "Hi, how are you?", likesCount: 12 },
-  //   { id: 2, message: "It's my first post!", likesCount: 8 },
-  //   { id: 3, message: "Yo!" },
-  //   { id: 4, message: "A History Lesson." },
-  //   { id: 5, message: "The Wrong Lesson of Munich!" },
-  //   { id: 6, message: "Will we learn our lesson?" },
-  // ];
-
   let postsElements = props.posts.map((post) => {
     return <Post id={post.id} key={post.id} message={post.message} />;
   });
+
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    alert(newPostElement.current.value);
+  }
 
   return (
     <div className={cl.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={cl.posts}>{postsElements}</div>
